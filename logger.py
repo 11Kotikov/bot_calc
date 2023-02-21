6cecee07d801a4ca7368a549a05b3e0c1365ede9
Andrey P <115166233+11Kotikov@users.noreply.github.com>
2023-02-21 19:26:17 +0300

    add logger

---
+++ logger.py
@@ -0,0 +1,15 @@
+import json
+import datetime
+
+
+def add_log(data):
+    with open('log.json', 'a') as log_writing:
+        dt = str(datetime.datetime.now())
+        log_writing.write('-'*6+ dt + '\n')
+        json.dump(data, log_writing, indent=4)
+        log_writing.write('-'*6+ '\n')
+
+
+def read_log():
+    with open('log.json', 'a') as log_reading:
+        print(*log_reading.readlines)
